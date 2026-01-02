952a7cf2ad52bcec5d4d7a14ee53cd2412e0e6b9
Felipe <felps134679@gmail.com>
2026-01-01 21:56:21 -0300

    configuração incial banvo de dados(sql) mudança de nome do projeto e Swagger

---
+++ ControleGastos/Data/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ControleGastos.Api.Data
+{
+    /// <summary>
+    /// Neste momento, apenas configura a conexão com o banco de dados. 
+    /// </summary>
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+       
+    }
+}
